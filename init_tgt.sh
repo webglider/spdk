3897969c9f26180bd0918f76748ef4c4c69edecc
Midhul <midhul.v@gmail.com>
2021-01-20 20:18:46 -0500

    debug

--- init_tgt.sh
+++ init_tgt.sh
@@ -4,4 +4,4 @@ sudo scripts/rpc.py nvmf_create_transport -t TCP -c $((8*1024))
 
 sudo scripts/rpc.py nvmf_create_subsystem nqn.2020-07.com.midhul:null0 -a -s SPDK00000000000003 -d SPDK_Controller3
 sudo scripts/rpc.py nvmf_subsystem_add_ns nqn.2020-07.com.midhul:null0 Null0
-sudo scripts/rpc.py nvmf_subsystem_add_listener nqn.2020-07.com.midhul:null0 -t TCP -s 4420 -a 12.0.0.10
+sudo scripts/rpc.py nvmf_subsystem_add_listener nqn.2020-07.com.midhul:null0 -t TCP -s 4420 -a $1
